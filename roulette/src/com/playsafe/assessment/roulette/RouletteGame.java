b74f7a3e96fd2d2223e5f859e92e1e7ab6abf2d8
mahlori <mahlori.ngobeni@supergrp.com>
2020-10-15 17:10:07 +0200

    get results for bets

--- roulette/src/com/playsafe/assessment/roulette/RouletteGame.java
+++ roulette/src/com/playsafe/assessment/roulette/RouletteGame.java
@@ -4,7 +4,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Random;
+import java.math.BigDecimal;
+import java.util.*;
 
 public class RouletteGame {
 
@@ -14,15 +15,46 @@ public class RouletteGame {
 
     private void play() {
         File file = new File("src/input1.txt");
-        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+        Random random = new Random();
+        int number = random.nextInt(36) + 1;
+        try (BufferedReader br = new BufferedReader(new FileReader(file));
+             Scanner scanner = new Scanner(System.in)) {
+            Map<String, List<String>> betsMap = new HashMap<>();
+            System.out.println("Please enter bets for each person");
             String name;
             while ((name = br.readLine()) != null) {
                 System.out.print(name + " ");
-
+                String bet = scanner.nextLine();
+                if (Objects.isNull(betsMap.get(name))) {
+                    List<String> bets = new ArrayList<>();
+                    bets.add(bet);
+                    betsMap.put(name, bets);
+                } else {
+                    betsMap.get(name).add(bet);
+                }
             }
+            showResults(betsMap, number);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    private void showResults(Map<String, List<String>> betsMap, int number) {
+        System.out.println("Number: " + number);
+        betsMap.forEach((name, bets) -> {
+            System.out.println(name);
+            bets.forEach(betLine -> {
+                StringTokenizer st = new StringTokenizer(betLine, " ");
+                String bet = st.nextToken();
+                BigDecimal amount = new BigDecimal(st.nextToken());
+                getAmountWon(number, bet, amount);
+                BigDecimal amountWon = getAmountWon(4, bet, amount);
+                
+            });
+        });
+    }
+
+    private BigDecimal getAmountWon(int number, String bet, BigDecimal amount) {
+        return BigDecimal.ZERO;
+    }
 }
